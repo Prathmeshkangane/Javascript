4b8606af841b0e0aa576ca98745a0d1d7893e739
Prathmeshkangane <143053555+Prathmeshkangane@users.noreply.github.com>
2024-06-23 11:58:32 +0000

    05_strings.js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,26 @@
+const name = "Prathmesh"
+// console.log(name);
+
+
+const fullName = new String("Prathmesh-Kangane")
+const fullName1 =  (`   Prathmesh      `)
+// console.log(fullName.length);
+// console.log(fullName.toUpperCase());
+// console.log(fullName.charAt(16));
+// console.log(fullName.indexOf(`t`));
+// console.log(fullName.substring(0,4))
+// console.log(fullName.slice(-1,4));
+// console.log(fullName1)
+// console.log(fullName1.trim());
+// console.log(fullName1.replace(`h`,`k`));
+// console.log(name.includes(`Kangane`))
+// console.log(fullName.split(`-`));
+
+
+// ###############DECREPTED##################
+// console.log(fullName.blink());
+// console.log(fullName.bold())\
+// console.log(fullName.fontcolor("Red"));
+// let iname = name.italics()
+// console.log(iname);
+// ##########################################
