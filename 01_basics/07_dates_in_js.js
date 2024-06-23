e8917e8474452dbd05948b25f59b7f15d6be0e0b
Prathmeshkangane <143053555+Prathmeshkangane@users.noreply.github.com>
2024-06-23 12:44:31 +0000

    07_dates_in_js.js

---
+++ 01_basics/07_dates_in_js.js
@@ -0,0 +1,14 @@
+const myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+let myCreatedDate = new Date("01-14-2023")
+let  myTimeStamp = Date.now()
+// console.log(myTimeStamp);//Divide By 1000 to get time in seconds.
+// console.log(Date(myTimeStamp));
+// console.log(myCreatedDate.getTime());
+console.log(myCreatedDate.getMonth()+1);
