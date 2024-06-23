d3185b81b7bcd2df13487124f7f3b3a46d5d573b
Prathmeshkangane <143053555+Prathmeshkangane@users.noreply.github.com>
2024-06-23 13:10:21 +0000

    01_arrays.js

---
+++ 02_basics.js/01_arrays.js
@@ -0,0 +1,9 @@
+// JavaScript array-copy operations create ****shallow copies**** similar in heaps
+// .join converts array into strings
+// Diffference between Slice and Splice
+const myArr = [0,1,2,3,4,5,6]
+myArr.push(7)
+console.log(myArr);
+console.log(myArr.pop());
+console.log(myArr.includes(9));
+console.log(myArr);
