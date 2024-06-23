febe6ba2fcf1adc64638bfe69aa683a3834f21b6
Prathmeshkangane <143053555+Prathmeshkangane@users.noreply.github.com>
2024-06-23 05:24:23 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountID = 145236
+let accountEmail = "PK@gmail.com"
+var accountPassword = "12345"
+account_city = "Kalyan"
+
+// accountID = 4444444
+
+// console.log(accountID)
+accountEmail = "PK1@gmail.com"
+accountPassword = "123456789"
+account_city = "Mumbai"
+console.table([accountEmail,accountPassword,account_city])
